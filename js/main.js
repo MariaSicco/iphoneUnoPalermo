53fd6aaae02ac3e968665adb302b522828a10f96
MariaSicco <70549056+MariaSicco@users.noreply.github.com>
2026-04-28 21:24:49 -0300

    first commit

--- js/main.js
+++ js/main.js
@@ -319,3 +319,20 @@ document.addEventListener('DOMContentLoaded', () => {
   bindCursorFollower();
   bindMobileMenu();
 });
+
+  const btn = document.getElementById("scrollTopBtn");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+      btn.classList.add("show");
+    } else {
+      btn.classList.remove("show");
+    }
+  });
+
+  btn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
